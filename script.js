79e07385cd4e4bd6afcf58a9ae2adb34022ab06d
Estefania Salazar Valencia <estefaniasalazarv@iesanagustin.edu.co>
2022-04-09 00:25:35 -0500

    Tarea_1

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let totalC = document.getElementById('input1');
 let porcentajeP = document.getElementById('input2');
 let calcular = document.getElementById('calcular')
-
+let div = document.getElementById('div')
 
 
 calcular.onclick=function(){
     const input1= totalC.value
     const input2 = porcentajeP.value * totalC.value / 100;
-    document.querySelector('#text1').innerHTML += totalC.value;
-    document.querySelector('#text2').innerHTML += input2;
     suma = parseFloat(input1)+parseFloat(input2);
-    document.querySelector('#text3').innerHTML += suma
+    div.innerHTML = `
+    <p id="text1">Total de la cuenta: ${totalC.value} </p>
+    <p id="text2">Propina: ${input2} </p>
+    <p id="text3">Total a pagar: ${suma} </p>`
 }
 
 
